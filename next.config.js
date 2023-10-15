c322e4ddf2e087ee028e197da74258e5c3791ccc
khitomi <k.hitomi@n-plants.biz>
2023-10-15 11:01:58 +0900

    mui import

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withPWA = require('next-pwa')({
+  dest: 'public',
+})
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+module.exports = withPWA({
+  reactStrictMode: false, // trueにするとuseEffectが2回呼び出される
+  swcMinify: true,
+  experimental: {
+    appDir: true,
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+        },
+      ],
+    })
+    return config
+  },
+  images: {
+    domains: ['127.0.0.1', 'localhost', '0.0.0.0'],
+  },
+})
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,55 @@
+"use client";
+
 import Image from 'next/image'
 import styles from './page.module.css'
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail'
+import CloseIcon from '@mui/icons-material/Close'
+import DeleteIcon from '@mui/icons-material/DeleteOutlined'
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material'
+import {
+  DataGrid,
+  GridActionsCellItem,
+  GridColDef,
+  GridRowId,
+  GridRowsProp,
+  jaJP,
+} from '@mui/x-data-grid'
+import { useCallback, useContext, useEffect, useState } from 'react'
+
 
 export default function Home() {
+  const Aa = () => {
+
+    const theme = useTheme()
+    const fullScreen = useMediaQuery(theme.breakpoints.down('md'))
+  
+    const [addAccountModal, setAddAccountModal] = useState<boolean>(false)
+  
+    const [selectedDeleteAccountModal, setSelectedDeleteAccountModal] = useState<boolean>(false)
+  
+    const [rows, setRows] = useState<GridRowsProp>([])
+    const [columns, setColumns] = useState<GridColDef[]>([])
+  
+    const [appList, setAppList] = useState<any[]>([])
+    const [billingList, setBillingList] = useState<any[]>([])
+    const [planList, setPlanList] = useState<any[]>([])
+  
+    const [paginationModel, setPaginationModel] = useState({
+      page: 0,
+      pageSize: 100,
+    })
+  }
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
